f32bdc6384db5b379f2ef6ab60dacf5eb4f2d178
altafan <pietro@vulpem.com>
2020-05-01 13:36:12 +0200

    Add FeeBalance rpc proto and implementation

--- src/services/operatorService.ts
+++ src/services/operatorService.ts
@@ -1,21 +1,27 @@
 import grpc from 'grpc';
+import { networks } from 'liquidjs-lib';
 
 import { OperatorService } from '../proto/operator_grpc_pb';
 import {
   DepositAddressReply,
   FeeDepositAddressReply,
+  FeeBalanceReply,
 } from '../proto/operator_pb';
 import Markets from '../models/markets';
 import { DBInterface } from '../db/datastore';
 import { VaultInterface } from '../components/vault';
 import { CrawlerInterface, CrawlerType } from '../components/crawler';
+import Balance from '../components/balance';
 
 class Operator {
+  depositsEnabled = true;
+
   constructor(
     private datastore: DBInterface,
     private vault: VaultInterface,
     private crawler: CrawlerInterface,
-    private network: string
+    private network: string,
+    private explorer: string
   ) {}
 
   async depositAddress(
@@ -23,6 +29,14 @@ class Operator {
     callback: grpc.sendUnaryData<DepositAddressReply>
   ): Promise<void> {
     try {
+      if (!this.depositsEnabled) {
+        throw {
+          code: grpc.status.UNIMPLEMENTED,
+          name: 'NOT_ENABLED',
+          message: 'Deposits are temporary disabled',
+        };
+      }
+
       const model = new Markets(this.datastore.markets);
       const latestMarket = await model.getLastMarket();
       const latestDerivationIndex = latestMarket.derivationIndex;
@@ -62,10 +76,35 @@ class Operator {
         isFeeAccount
       );
 
-      this.crawler.start(CrawlerType.BALANCE, feeWallet.address);
-
       const reply = new FeeDepositAddressReply();
       reply.setAddress(feeWallet.address);
+      callback(null, reply);
+    } catch (e) {
+      return callback(e, null);
+    }
+  }
+
+  async feeBalance(
+    _: any,
+    callback: grpc.sendUnaryData<FeeBalanceReply>
+  ): Promise<void> {
+    try {
+      const derivationIndex = 0;
+      const isFeeAccount = true;
+      const feeWallet = this.vault.derive(
+        derivationIndex,
+        this.network,
+        isFeeAccount
+      );
+
+      const b = new Balance(this.datastore.unspents, this.explorer);
+      const bitcoinAssetHash = (networks as any)[this.network].assetHash;
+      const feeAccountBalance = (
+        await b.fromAsset(feeWallet.address, bitcoinAssetHash)
+      )[bitcoinAssetHash].balance;
+
+      const reply = new FeeBalanceReply();
+      reply.setBalance(feeAccountBalance);
       callback(null, reply);
     } catch (e) {
       return callback(e, null);
