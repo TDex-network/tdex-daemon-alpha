e025b099feee8f3ee184ad28d82356026086a4c0
altafan <pietro@vulpem.com>
2020-05-01 17:38:56 +0200

    Revert changes

--- src/grpc/tradeServer.ts
+++ src/grpc/tradeServer.ts
@@ -6,8 +6,7 @@ import { DBInterface } from '../db/datastore';
 import { VaultInterface } from '../components/vault';
 
 export default class TradeServer {
-  server!: Server;
-  tradeService: Trade;
+  server: Server;
 
   constructor(
     private datastore: DBInterface,
@@ -16,21 +15,18 @@ export default class TradeServer {
     private explorer: string,
     private logger: Logger
   ) {
-    this.tradeService = new Trade(
+    this.server = new Server();
+    const service = new Trade(
       this.datastore,
       this.vault,
       this.network,
-      this.explorer
+      this.explorer,
+      this.logger
     );
+    this.server.addService(TradeService, service as any);
   }
 
-  // Beacause of https://github.com/grpc/grpc/issues/7031
-  // to be able to start and stop a server  from code we need to
-  // instantiate a new Server instance each time.
   listen(host: string, port: number): void {
-    this.server = new Server();
-    this.server.addService(TradeService, this.tradeService as any);
-
     const bindCode = this.server.bind(
       `${host}:${port}`,
       ServerCredentials.createInsecure()
@@ -46,8 +42,6 @@ export default class TradeServer {
     return new Promise((resolve) =>
       this.server.tryShutdown(() => {
         this.logger.info('Trader gRPC server completed shutdown');
-        // manually update the server status
-        (this.server as any).started = false;
         resolve();
       })
     );
